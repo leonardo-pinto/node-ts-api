3b6ce528ac92c2d97d6c05858090fab9b5ca99d5
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-04-12 21:43:15 -0300

    test: ensure EmailValidator is called with correct parameters

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -94,4 +94,20 @@ describe('SignUpController', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new InvalidParamError('email'))
   })
+
+  test('Should call EmailValidator with correct parameters', () => {
+    const { sut, emailValidatorStub } = makeSut()
+    const emailValidatorSpy = jest.spyOn(emailValidatorStub, 'isValid')
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    sut.handle(httpRequest)
+    expect(emailValidatorSpy).toHaveBeenCalledWith('any_email')
+  })
 })
