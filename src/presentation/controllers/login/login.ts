2eb7e265f062957b27130ca378cc30978af2dc72
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-08-02 18:55:21 -0300

    feat: ensure LoginController returns 400 if params are missing

---
+++ src/presentation/controllers/login/login.ts
@@ -0,0 +1,16 @@
+import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+import { badRequest, ok } from '../../helpers/http-helper'
+import { MissingParamError } from '../../errors'
+
+export class LoginController implements Controller {
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    if (!httpRequest.body.email) {
+      return await new Promise(resolve => resolve(badRequest(new MissingParamError('email'))))
+    }
+    if (!httpRequest.body.password) {
+      return await new Promise(resolve => resolve(badRequest(new MissingParamError('password'))))
+    }
+
+    return ok('any')
+  }
+}
