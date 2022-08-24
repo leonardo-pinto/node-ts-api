e4c6c035bcec7fa0c351b28297a79900f537ebe3
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-08-24 06:49:35 -0300

    test: ensure BcryptAdapter returs true when compare succeeds

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -46,10 +46,9 @@ describe('BcryptAdapter', () => {
     expect(compareSpy).toHaveBeenCalledWith('any_value', 'any_hash')
   })
 
-  test('Should call compare with correct parameters', async () => {
+  test('Should return true when compare succeeds', async () => {
     const sut = makeSut()
-    const compareSpy = jest.spyOn(bcrypt, 'compare')
-    await sut.compare('any_value', 'any_hash')
-    expect(compareSpy).toHaveBeenCalledWith('any_value', 'any_hash')
+    const isValid = await sut.compare('any_value', 'any_hash')
+    expect(isValid).toBeTruthy()
   })
 })
