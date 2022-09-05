9aebde80088ce28e5b5347657e35111c31f9128e
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-09-05 06:40:56 -0300

    refactor: move login and signup controllers to separate folder

--- src/presentation/controllers/login/signup/signup-controller.spec.ts
+++ src/presentation/controllers/login/signup/signup-controller.spec.ts
@@ -1,7 +1,7 @@
 import { SignUpController } from './signup-controller'
-import { EmailInUseError, MissingParamError, ServerError } from '../../errors'
+import { EmailInUseError, MissingParamError, ServerError } from '../../../errors'
 import { AccountModel, AddAccount, AddAccountModel, HttpRequest, Validation } from './signup-protocols-controller'
-import { ok, serverError, badRequest, forbidden } from '../../helpers/http/http-helper'
+import { ok, serverError, badRequest, forbidden } from '../../../helpers/http/http-helper'
 import { Authentication, AuthenticationModel } from '../login/login-protocols-controller'
 
 const makeAddAccount = (): AddAccount => {
