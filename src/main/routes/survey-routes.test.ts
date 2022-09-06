97a1a7560e982c5faa5e1798b8a4c6468ed3d14d
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-09-06 07:00:53 -0300

    feat: add AddSurvey composition

---
+++ src/main/routes/survey-routes.test.ts
@@ -0,0 +1,38 @@
+import { Collection } from 'mongodb'
+import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import app from '../config/app'
+
+let surveyCollection: Collection
+
+describe('Survey Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+  })
+
+  describe('POST /surveys', () => {
+    test('Should return 204 on add survey success', async () => {
+      await request(app)
+        .post('/api/surveys')
+        .send({
+          question: 'Question',
+          answers: [
+            {
+              image: 'image',
+              answer: 'Answer 1'
+            }
+          ]
+        })
+        .expect(204)
+    })
+  })
+})
