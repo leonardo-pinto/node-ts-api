a824ed900d67abb04524301ffa936c298e7780b4
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-09-07 12:05:39 -0300

    test: ensure JwtAdapter returns a decrypted value on verify success

--- src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
@@ -37,5 +37,13 @@ describe('Jwt Adapter', () => {
       await sut.decrypt('any_token')
       expect(verifySpy).toHaveBeenCalledWith('any_token', 'secret')
     })
+
+    test('Should return a value on verify success', async () => {
+      const sut = makeSut()
+      const value = await sut.decrypt('any_id')
+      expect(value).toBe('any_value')
+    })
+
+    test
   })
 })
