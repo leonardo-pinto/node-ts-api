9896a8e91fdae6ebb913e733b3a3c3eb6eca7c00
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-09-08 08:57:28 -0300

    test: ensure SurveyMongoRepository returns an empty list if there are no surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -72,32 +72,10 @@ describe('Survey Mongo Repository', () => {
       expect(surveys[1].question).toBe('other_question')
     })
 
-    test('Should load all surveys on success', async () => {
-      await surveyCollection.insertMany([
-        {
-          id: 'any_id',
-          question: 'any_question',
-          answers: [{
-            image: 'any_image',
-            answer: 'any_answer'
-          }],
-          date: new Date()
-        },
-        {
-          id: 'other_id',
-          question: 'other_question',
-          answers: [{
-            image: 'other_image',
-            answer: 'other_answer'
-          }],
-          date: new Date()
-        }
-      ])
+    test('Should load empty list', async () => {
       const sut = makeSut()
       const surveys = await sut.loadAll()
-      expect(surveys.length).toBe(2)
-      expect(surveys[0].question).toBe('any_question')
-      expect(surveys[1].question).toBe('other_question')
+      expect(surveys.length).toBe(0)
     })
   })
 })
