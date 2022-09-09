466125deb23677e19b6514ab8de2b6d06b6650c1
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-09-09 06:53:20 -0300

    test: ensure DbLoadSurveyById calls LoadSurveyByIdRepository with correct values

---
+++ src/data/usecases/load-survey-by-id/db-load-survey-by-id.spec.ts
@@ -0,0 +1,62 @@
+import { LoadSurveyByIdRepository } from '@/data/protocols/db/survey/load-survey-by-id'
+import { SurveyModel } from '@/domain/models/survey'
+import MockDate from 'mockdate'
+import { DbLoadSurveyById } from './db-load-survey-by-id'
+
+type SutTypes = {
+  sut: DbLoadSurveyById
+  loadSurveyByIdRepositoryStub: LoadSurveyByIdRepository
+}
+
+const makeFakeSurvey = (): SurveyModel => ({
+  id: 'any_id',
+  question: 'any_question',
+  answers: [{
+    image: 'any_image',
+    answer: 'any_answer'
+  }],
+  date: new Date()
+})
+
+const makeLoadSurveyByIdRepository = (): LoadSurveyByIdRepository => {
+  class LoadSurveyByIdRepositoryStub implements LoadSurveyByIdRepository {
+    async loadById (_id: string): Promise<SurveyModel> {
+      return await new Promise(resolve => resolve(makeFakeSurvey()))
+    }
+  }
+
+  return new LoadSurveyByIdRepositoryStub()
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveyByIdRepositoryStub = makeLoadSurveyByIdRepository()
+  const sut = new DbLoadSurveyById(loadSurveyByIdRepositoryStub)
+
+  return {
+    loadSurveyByIdRepositoryStub,
+    sut
+  }
+}
+
+describe('DbLoadSurveyById', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
+  test('Should call LoadSurveyByIdRepository with correct parameters', async () => {
+    const { sut, loadSurveyByIdRepositoryStub } = makeSut()
+    const loadByIdSpy = jest.spyOn(loadSurveyByIdRepositoryStub, 'loadById')
+    await sut.loadById('any_id')
+    expect(loadByIdSpy).toHaveBeenCalledWith('any_id')
+  })
+
+  test('Should return a list of surveys on success', async () => {
+    const { sut } = makeSut()
+    const surveys = await sut.load()
+    expect(surveys).toEqual(makeFakeSurveys())
+  })
+})
