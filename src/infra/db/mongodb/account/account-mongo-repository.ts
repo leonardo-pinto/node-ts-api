4462db3e8e0596ff0e57abba85847e1e3f073366
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-09-12 07:08:22 -0300

    refactor: organize project files

--- src/infra/db/mongodb/account/account-mongo-repository.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.ts
@@ -1,7 +1,7 @@
 import { ObjectId } from 'mongodb'
 import { LoadAccountByEmailRepository, UpdateAccessTokenRepository, AddAccountRepository, LoadAccountByTokenRepository } from '@/data/protocols/db/account'
 import { AccountModel } from '@/domain/models/account'
-import { AddAccountModel } from '@/domain/usecases/add-account'
+import { AddAccountModel } from '@/domain/usecases/account/add-account'
 import { MongoHelper } from '../helpers/mongo-helper'
 
 export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository, UpdateAccessTokenRepository, LoadAccountByTokenRepository {
