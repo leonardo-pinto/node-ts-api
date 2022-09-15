5b310c91c656bfb2796d653258f4680d10a2771e
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-09-15 18:57:57 -0300

    docs: add login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,27 @@
+import { loginPath } from './paths/login-path'
+import { accountSchema } from './schemas/account-schema'
+import { loginSchema } from './schemas/login-params-schema'
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Clean Node API',
     description: 'API to create surveys',
     version: '1.0'
+  },
+  servers: [
+    {
+      url: '/api'
+    }
+  ],
+  tags: [{
+    name: 'Login'
+  }],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginSchema
   }
 }
---
+++ src/main/docs/schemas/login-params-schema.ts
@@ -0,0 +1,12 @@
+export const loginSchema = {
+  type: 'object',
+  properties: {
+    email: {
+      type: 'string'
+    },
+    password: {
+      type: 'string'
+    },
+    redquired: ['email', 'password']
+  }
+}
